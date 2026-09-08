863e44c7fb18e1edae11820d1ef3e00afa20de99
epoundor <freedausstanda@gmail.com>
2026-09-08 15:00:04 +0100

    feat: add Hwekan Media project and restructure project list exports

--- projects.js
+++ projects.js
@@ -1,31 +1,38 @@
 export const PROJECTS = {
-  "StakPass": {
-    "description": "A web application managing event and ticket sales.",
-    "link": "https://stakpass.com",
-    "image": "/images/projects/StakPass.png"
+  StakPass: {
+    description: "A web application managing event and ticket sales.",
+    link: "https://stakpass.com",
+    image: "/images/projects/StakPass.png",
   },
-  "Econseil":{
-    "description": "A web application for the Béninese government to manage the council of ministers.",
-    "link": "https://www.econseil.gouv.bj/",
-    "image": "/images/projects/Econseil.png"
+  Econseil: {
+    description:
+      "A web application for the Béninese government to manage the council of ministers.",
+    link: "https://www.econseil.gouv.bj/",
+    image: "/images/projects/Econseil.png",
   },
-  "ExcaliDraw Clone":{
-    "description": "A real-time collaborative drawing application.",
-    "link": "https://github.com/The-Open-Mafia/excalidraw_clone",
-    "image": "/images/projects/excalidraw_clone.png"
+  "Hwekan Media": {
+    description:
+      "Espace de valorisation, de narration et de documentation des dynamiques créatives béninois.",
+    link: "https://hwekan.com/",
+    image: "/images/projects/hwekan.png",
   },
-  "Vue Fetch":{
-    "description": "A package that simplifies making HTTP requests in Vue.js.",
-    "link": "https://github.com/epoundor/vue-fetch",
-    "image": "/images/projects/vue-fetch.png"
+  "ExcaliDraw Clone": {
+    description: "A real-time collaborative drawing application.",
+    link: "https://github.com/The-Open-Mafia/excalidraw_clone",
+    image: "/images/projects/excalidraw_clone.png",
   },
-  "Personal Website": {
-    "description": "This website.",
-    "link": "https://epoundor.me",
-    "image": "/images/projects/Personal_Website.png"
-  }
+  "Vue Fetch": {
+    description: "A package that simplifies making HTTP requests in Vue.js.",
+    link: "https://github.com/epoundor/vue-fetch",
+    image: "/images/projects/vue-fetch.png",
+  },
 };
 
 export const ALL_PROJECTS = {
-  ...PROJECTS
-}
+  "Personal Website": {
+    description: "This website.",
+    link: "https://epoundor.me",
+    image: "/images/projects/Personal_Website.png",
+  },
+  ...PROJECTS,
+};
